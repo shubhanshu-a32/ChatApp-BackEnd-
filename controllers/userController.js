1ea14018ba8dda2c791cc13a21769365c81abff4
Shubhanshu Upadhyay <osmwow17@gmail.com>
2025-06-21 17:49:55 +0530

    Backend commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,7 +5,7 @@ import redisClient from '../config/redis.js';
 export const getMe = async (req, res) => {
     try {
         const user = await User.findById(req.user.id).select('-password');
-        res.status(200).json(user);
+        res.status(200).json({ user });
     } catch (error) {
         console.error('Error fetching user:', error);
         res.status(500).json({ message: 'Failed to fetch user' });
